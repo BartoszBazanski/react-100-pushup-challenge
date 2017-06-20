122c76053a46d9882e3db0cf197a29e47e1141f3
Bartosz Bazański <b.bazanski@visuality.pl>
2017-06-20 16:21:40 +0200

    Add first functionality: toggle active state of workouts

--- app/components/WorkoutDay/index.js
+++ app/components/WorkoutDay/index.js
@@ -1,12 +1,28 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component, PropTypes } from 'react';
 import H3 from '../H3';
 import styles from './styles.scss';
 
-export default class WorkoutDay extends Component {// eslint-disable-line react/prefer-stateless-function
+export default class WorkoutDay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDone: false,
+    };
+    this.toggleActive = this.toggleActive.bind(this);
+  }
+  toggleActive() {
+    const { isDone } = this.state;
+    this.setState({
+      isDone: !isDone,
+    });
+  }
   render() {
     const { header, workout } = this.props;
+    const { isDone } = this.state;
     return (
-      <ul className={styles.workoutDay} >
+      <ul className={isDone ? styles.workoutDayActive : styles.workoutDay} onClick={this.toggleActive}>
         <H3>{header}</H3>
         {workout.map((pushups, id) => (
           <li key={id} className={styles.workoutRound}>{pushups}</li>
